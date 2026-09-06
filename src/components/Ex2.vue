733ca53247c6c404f2255662df90a371fe904617
nxpour2025-sketch <nx.pour.2025@smu.edu.sg>
2026-09-06 21:27:51 +0800

    ex1,2 try5

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,11 +3,12 @@ import { ref } from 'vue'
 
 // Add code here
 const me = ref('/assets/me.png')
-const width = ref('600')
-const height = ref('400')
+const x = ref('600')
+const y = ref('400')
 
+import wad from "/assets/wad.png"
 const obj = {
-  src: "../assets/wad.png",
+  src: wad,
   width: 400,
   height: 400,
 }
@@ -16,7 +17,7 @@ const obj = {
 <template>
   <!-- Add code here -->
    <!--multiple binding-->
-   <img v-bind:src="me" :width="width" :height="height" />
+   <img v-bind:src="me" :width="x" :height="y" />
    <hr />
    <!--single binding-->
    <img v-bind="obj" />
